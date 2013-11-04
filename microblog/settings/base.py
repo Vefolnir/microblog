3a4b37a071783d0b02d2ffed5f2f2a1d108c6f83
Vefolnir <erion.jonatan@gmail.com>
2013-11-04 23:25:09 +0000

    requirements fix

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -15,9 +15,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': dj_database_url.config()
-}
+DATABASES['default'] = dj_database_url.config()
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
