6d93d38b9d81240a26d7a05ac2372cecb9a737f0
Vefolnir <erion.jonatan@gmail.com>
2013-11-04 19:58:07 +0000

    activated the admin

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -64,7 +64,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    root("assets")
+    root("assets"),
 )
 
 # List of finder classes that know how to find static files in
@@ -101,7 +101,7 @@ ROOT_URLCONF = 'microblog.urls'
 WSGI_APPLICATION = 'microblog.wsgi.application'
 
 TEMPLATE_DIRS = (
-    root("templates")
+    root("templates"),
 )
 
 
