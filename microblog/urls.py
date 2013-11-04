7d8116f4c29df13c26b376a343c752c9f4f009ab
Vefolnir <erion.jonatan@gmail.com>
2013-11-04 21:28:25 +0000

    first templates added-fix1

--- microblog/urls.py
+++ microblog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r"^$", TemplateView.as_view(template_name="index.html")),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
