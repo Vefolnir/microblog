6ae335291498f36745b75c1ee3a77080ec9b1379
Vefolnir <erion.jonatan@gmail.com>
2013-11-04 21:23:51 +0000

    first templates added

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 admin.autodiscover()
 
@@ -10,6 +12,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r"^$", TemplateView.as_view(template_name="index.html")),
 
     url(r'^admin/', include(admin.site.urls)),
 )
