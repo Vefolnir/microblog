9f016b93dbd1f1fc799fc1187a7b0cdb8413ee4d
Vefolnir <erion.jonatan@gmail.com>
2013-11-05 18:07:07 +0000

    database release

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+	date_hierarchy = "creado"
+	fields = ("publicado", "titulo", "contenido")
+	list_display = ["publicado", "titulo", "editado"]
+	list_display_links = ["titulo"]
+	list_editable = ["publicado"]
+	list_filter =["publicado", "editado"]
+
+
+
+admin.site.register(Post, PostAdmin)
 
--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,22 @@
+# -*- encoding: utf-8 -*-
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
 
+
 class Post(models.Model):
-	creado = models.DateTimeField(auto_now_add=True, editable=False)
-	editado = models.DateTimeField(auto_now_add=True, editable=False)
-	titulo = models.CharField(max_length=255)
-	slug = models.SlugField(max_length=255, blank=True, default='')
-	contenido = models.TextField()
-	publicado = models.BooleanField(default=True)
+    creado = models.DateTimeField(auto_now_add=True, editable=False)
+    editado = models.DateTimeField(auto_now_add=True, editable=False)
+    titulo = models.CharField(max_length=255, verbose_name=u'Título')
+    slug = models.SlugField(max_length=255, blank=True, default='')
+    contenido = models.TextField()
+    publicado = models.BooleanField(default=True)
+    autor = models.ForeignKey(User, related_name="posts")
 
-	def __unicode__(self):
-		return self.titulo
+    def __unicode__(self):
+        return self.titulo
 
-	def save(self, *args, **kwargs):
-		if not self.slug:
-			self.slug == slugify(self.title)
-		super(Post, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.titulo)
+        super(Post, self).save(*args, **kwargs)
